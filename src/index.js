224f9e6cafa1551a1d2db67e8e736fe998f3703b
Your Name <naimur202163@gmail.com>
2022-08-28 08:32:18 +0600

    reducers added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
+import { Provider } from 'react-redux/es/exports';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+   
     <App />
+   
+   
   </React.StrictMode>
 );
